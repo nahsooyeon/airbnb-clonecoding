4dc4a75c269243c8237f2731d9de828cdc88725b
nahsooyeon <nahsooyeon@gmail.com>
2022-11-05 13:14:45 +0900

    🔨 수정: 데이터독 로그 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,12 @@
-import '../styles/globals.scss';
+import "../styles/globals.scss";
+import { datadogLogs } from "@datadog/browser-logs";
+
+datadogLogs.init({
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
+  site: "datadoghq.com",
+  forwardErrorsToLogs: true,
+  sampleRate: 100,
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
