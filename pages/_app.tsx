dd1b72fbec5e91a2abb4475e5fc98fe62d474363
nahsooyeon <nahsooyeon@gmail.com>
2022-11-05 13:41:39 +0900

    🔨 수정: 데이터독 환경변수 추가 및 RUM 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../styles/globals.scss";
 import { datadogLogs } from "@datadog/browser-logs";
+import { datadogRum } from "@datadog/browser-rum";
+import { useEffect } from "react";
 
 datadogLogs.init({
   clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN,
@@ -8,7 +10,26 @@ datadogLogs.init({
   sampleRate: 100,
 });
 
+datadogRum.init({
+  applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID,
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_RUM_TOKEN,
+  site: "datadoghq.com",
+  service: "sooyeon-datadog-test",
+  env: "prod",
+  // Specify a version number to identify the deployed version of your application in Datadog
+  // version: '1.0.0',
+  sampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: "mask-user-input",
+});
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    datadogRum.startSessionReplayRecording();
+  }, []);
   return <Component {...pageProps} />;
 }
 
